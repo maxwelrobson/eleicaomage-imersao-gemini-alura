7d67b963e136fab1e83730076cba20392eea8ce8
Maxwel Robson <maxps2@live.com>
2024-09-06 22:04:19 -0300

    .

--- Imersão/app.js
+++ Imersão/app.js
@@ -1,25 +1,52 @@
 function pesquisar() {
     // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
+  let section = document.getElementById("resultados-pesquisa");
+  
+    //
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte</p>"
+    return
+  }
+  
+  campoPesquisa = campoPesquisa.toLowerCase()
   
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
-    let resultados = "";
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
   
     // Itera sobre cada dado na lista de dados (assumindo que 'dados' é um array de objetos)
-    for (let dado of dados) {
-      // Constrói o HTML para cada resultado, formatando os dados do objeto
+  for (let dado of dados) {
+
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+      //se titulo includes campoPesquisa 
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        // Constrói o HTML para cada resultado, formatando os dados do objeto
       resultados += `
-        <div class="item-resultado">
-          <h2><a href="#">${dado.titulo}</a></h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href=${dado.link} target="_blank">Mais informações</a>
-        </div>
-      `;
-    }
+      <div class="item-resultado">
+        <h2><a href="#">${dado.titulo}</a></h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href=${dado.link} target="_blank">Mais informações</a>
+      </div>
+    `;
+      }
+          //então, faça...
+  
+      
+  }
+  
+  if (!resultados) {
+      resultados = "<p> Nada foi encontrado </p>"
+  }
   
     // Atribui a string com os resultados HTML à seção, substituindo o conteúdo anterior
     section.innerHTML = resultados;
-  }
+}
 
 
 
