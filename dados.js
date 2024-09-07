6cf78637137c18843a9050cb4e3d0b42ccdf2b7f
Maxwel Robson <maxps2@live.com>
2024-09-07 18:03:47 -0300

    .

--- dados.js
+++ dados.js
@@ -22,9 +22,70 @@ tags: "prefeito"
     },
 
     {
-        titulo: "Walter Barbosa",
-        partido: "PSB",
-        numeroDeUrna : "40",
-        tags: "prefeito"
+        titulo: "Adeildo da bike",
+        partido: "REDE",
+        numeroDeUrna : "18999",
+        tags: "Vereador(a)"
     },
+
+    {
+        titulo: "Ademar da Saúde",
+        partido: "DC",
+        numeroDeUrna : "27027",
+        tags: "Vereador(a)"
+    },
+
+    {
+        titulo: "Ademar Oliveira",
+        partido: "PDSB",
+        numeroDeUrna : "45287",
+        tags: "Vereador(a)"
+    },
+
+    {
+        titulo: "Adilson Capacia",
+        partido: "PL",
+        numeroDeUrna : "22122",
+        tags: "Vereador(a)"
+    },
+
+    {
+        titulo: "Adriane Forte",
+        partido: "PL",
+        numeroDeUrna : "22022",
+        tags: "Vereador(a)"
+    },
+
+    {
+        titulo: "Agatha Rodrigues",
+        partido: "MDB",
+        numeroDeUrna : "15565",
+        tags: "Vereador(a)"
+    },
+
+    {
+        titulo: "Aguiana Charnase",
+        partido: "AGIR",
+        numeroDeUrna : "36777",
+        tags: "Vereador(a)"
+    },
+
+    {
+        titulo: "Álvaro Alencar",
+        partido: "PT",
+        numeroDeUrna : "13456",
+        tags: "Vereador(a)"
+    },
+
+    {
+        titulo: "",
+        partido: "",
+        numeroDeUrna : "",
+        tags: "Vereador(a)"
+    },
+
+
+    
+
+
 ];
